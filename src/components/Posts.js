47d86cd84cb0343da645da57e5e6a5119b678735
adudley78 <dudleyadam@gmail.com>
2019-05-23 17:25:13 -0400

    Clean up default code in App.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Posts extends Component {
+class Posts extends Component {
     render() {
         return (
             <div>
@@ -9,3 +9,5 @@ export default class Posts extends Component {
         )
     }
 }
+
+export default Posts;
